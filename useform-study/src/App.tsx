26d8d9c25516aaf66a6fece3015fbec438284577
渡邉将太 <shotawebcoding@gmail.com>
2022-11-20 22:35:38 +0900

    Update App.tsx

--- useform-study/src/App.tsx
+++ useform-study/src/App.tsx
@@ -8,20 +8,27 @@ type Inputs = {
 
 function App() {
  
-    const { register, handleSubmit, setValue} = useForm<Inputs>();
+    const { register, handleSubmit, setValue, getValues,resetField } = useForm<Inputs>();
     
     const onSubmit: SubmitHandler<Inputs> = (data) => { 
         console.log(data.text);
     } 
 
     const changeTextValue = (value: string) => {
-    setValue("text", value);
+    const currentText = getValues("text")
+
+    setValue("text", currentText === "" ? value : `${currentText},${value}`);
+    };
+    
+    const resetTextValue = () => {
+    resetField("text");
   };
 
     return (
         <>
       <form onSubmit={handleSubmit(onSubmit)} style={{ margin: "50px" }}>
-        <input {...register("text")} style={{ margin: "20px" }} />
+                <input {...register("text")}  style={{ margin: "20px" }} placeholder='入力してください' />
+                
         <input type="submit" />
         <div>
           {["orange", "grape", "banana"].map((value) => {
@@ -32,10 +39,17 @@ function App() {
                 onClick={() => changeTextValue(value)}
               >
                 {value}
-              </button>
+                </button>
+                
             );
           })}
-        </div>
+                    <button
+            onClick={resetTextValue}
+            style={{ margin: "10px", background: "gray", color: "#fff" }}
+          >
+            クリア
+          </button>
+                </div>
       </form>
     </>
   );
